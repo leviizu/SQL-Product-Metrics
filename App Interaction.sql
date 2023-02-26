72baa7c43e347f70d9b2e7889a8ee48f51f10f87
Levy Shemtov <96123459+leviizu@users.noreply.github.com>
2023-02-26 17:57:14 +0100

    Update App Interaction.sql

--- App Interaction.sql
+++ App Interaction.sql
@@ -1,4 +1,3 @@
-
 SELECT
   all_spaces.space_id,
   os.users AS os_users,
@@ -13,36 +12,36 @@ FROM (--extracting all distinct spaces in database
   CROSS JOIN
     UNNEST (user_properties)
   WHERE
-    KEY = 'space_id') all_spaces
-  LEFT JOIN (--extracting users by their operating system
-    SELECT
-      value.string_value AS space_id,
-      COUNT(DISTINCT user_id) AS Users,
-      device.operating_system AS os
-    FROM
-      `office-life-169414.analytics_157635370.events_*`
-    CROSS JOIN
-      UNNEST (user_properties)
-    WHERE
-      KEY = 'space_id'
-    GROUP BY
-      space_id,
-      os)os
-  ON
-    all_spaces.space_id=os.space_id
-  LEFT JOIN (--extracting users by device language
-    SELECT
-      value.string_value AS space_id,
-      COUNT(DISTINCT user_id) AS Users,
-      SUBSTR(device.language,0,2) AS device_language
-    FROM
-      `office-life-169414.analytics_157635370.events_*`
-    CROSS JOIN
-      UNNEST (user_properties)
-    WHERE
-      KEY = 'space_id'
-    GROUP BY
-      space_id,
-      device_language)lang
-  ON
-    all_spaces.space_id=lang.space_id
+    key = 'space_id') all_spaces
+LEFT JOIN (--extracting users by their operating system
+  SELECT
+    value.string_value AS space_id,
+    COUNT(DISTINCT user_id) AS Users,
+    device.operating_system AS os
+  FROM
+    `office-life-169414.analytics_157635370.events_*`
+  CROSS JOIN
+    UNNEST (user_properties)
+  WHERE
+    key = 'space_id'
+  GROUP BY
+    space_id,
+    os)os
+ON
+  all_spaces.space_id=os.space_id
+LEFT JOIN (--extracting users by device language
+  SELECT
+    value.string_value AS space_id,
+    COUNT(DISTINCT user_id) AS Users,
+    SUBSTR(device.language,0,2) AS device_language
+  FROM
+    `office-life-169414.analytics_157635370.events_*`
+  CROSS JOIN
+    UNNEST (user_properties)
+  WHERE
+    key = 'space_id'
+  GROUP BY
+    space_id,
+    device_language)lang
+ON
+  all_spaces.space_id=lang.space_id
